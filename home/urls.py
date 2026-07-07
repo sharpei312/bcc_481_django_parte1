0ed9ca100c9dff676f48a5d15dbe094083cc461a
Tommy Rodriguez <tommyrz0512@gmail.com>
2026-07-06 20:41:38 -0500

    parte_1

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
+﻿from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("", include("home.urls")),
 ]
